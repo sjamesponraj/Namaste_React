a8ce491bafbcbb989e6598bd285324f6d18c42ca
James Ponraj <sjamesponraj@gmail.com>
2023-02-02 23:05:27 +0530

    Assignment2_Final

---
+++ assignment2/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const heading1 = React.createElement('h1', { className: 'main' }, 'Hello React')
+const heading2 = React.createElement(
+  'h2',
+  { className: 'h2', id: 'heading2' },
+  'Jesus Help me Out'
+)
+const root = ReactDOM.createRoot(document.querySelector('.root'))
+
+root.render([heading1, heading2])
